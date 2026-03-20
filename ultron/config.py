d6b09e401dfae4773d2d16d22924743f23a58d38
Ask-812 <arnavsk812@gmail.com>
2026-03-21 03:59:31 +0530

    [ULTRON] Before set_param: cell_mutation_rate

--- ultron/config.py
+++ ultron/config.py
@@ -4,7 +4,7 @@
     'starting_energy': 50,
     'energy_capacity': 50,
     'consumption_rate': 0.05,
-    'learning_rate': 0.15,
+    'learning_rate': 0.1,
     'prior_learning_rate': 0.001,
     'precision_learning_rate': 0.001,
     'update_cost_factor': 0.05,
