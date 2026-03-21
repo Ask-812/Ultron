4419cc95f3b2aae3bb17d41b20c392df21ae7c1e
Ask-812 <arnavsk812@gmail.com>
2026-03-21 06:22:52 +0530

    [ULTRON] Before set_param: division_energy_threshold

--- ultron/config.py
+++ ultron/config.py
@@ -3,7 +3,7 @@
     'model_dim': 64,
     'starting_energy': 50,
     'energy_capacity': 50,
-    'consumption_rate': 1.5,
+    'consumption_rate': 0.03,
     'learning_rate': 0.05,
     'prior_learning_rate': 0.001,
     'precision_learning_rate': 0.001,
